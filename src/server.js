b613f51e4ec63356d87ac7e28acbe7bca88ec58b
Satria Sapta <satriasapta48@gmail.com>
2023-10-02 00:23:33 +0700

    create notes schema and validator

--- src/server.js
+++ src/server.js
@@ -14,7 +14,6 @@ const init = async () => {
     },
   });
 
-
   await server.register({
     plugin: notes,
     options: {
---
+++ src/validator/notes/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const NotesValidator = {
+  validateNotePayload: (payload) => {
+    const validationResult = NotePayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new Error(validationResult.error.message);
+    }
+  },
+};
